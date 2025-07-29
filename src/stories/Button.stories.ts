1778ea3b35c39d9e346b8b87d6285b448dd349c9
Y-jae-heon <yeomjae5766@gmail.com>
2025-07-29 15:30:39 +0900

    update: storybook button update / input add

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -27,6 +27,10 @@ const meta = {
 				'link',
 			],
 		},
+		size: {
+			control: 'select',
+			options: ['default', 'sm', 'lg', 'icon'],
+		},
 	},
 	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 	args: { onClick: fn() },
---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,86 @@
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+const meta: Meta<typeof AlertDialog> = {
+	title: 'Atom/Input',
+	component: Input,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+	argTypes: {},
+};
+
+export default meta;
+
+// Custom 타입 선언
+// type CustomArgs = {
+// 	title: string;
+// 	description: string;
+// 	variant?: 'default' | 'destructive';
+// 	backgroundColor: string;
+// 	icon: boolean;
+// };
+
+export const Default: StoryObj<typeof Input> = {
+	args: {
+		placeholder: 'Input',
+	},
+};
+
+// export const CustomAlert: StoryObj<CustomArgs> = {
+// 	args: {
+// 		title: 'Alert Title',
+// 		description: 'Alert description',
+// 		backgroundColor: 'bg-white',
+// 	},
+// 	argTypes: {
+// 		backgroundColor: {
+// 			control: 'select',
+// 			options: [
+// 				'bg-white',
+// 				'bg-red-500',
+// 				'bg-blue-500',
+// 				'bg-green-500',
+// 				'bg-yellow-500',
+// 				'bg-purple-500',
+// 				'bg-pink-500',
+// 				'bg-orange-500',
+// 				'bg-gray-500',
+// 				'bg-black',
+// 			],
+// 		},
+// 	},
+// 	render: ({ title, description, backgroundColor }) => (
+// 		<AlertDialog>
+// 			<AlertDialogTrigger>
+// 				<Button>Show Dialog</Button>
+// 			</AlertDialogTrigger>
+// 			<AlertDialogContent className={cn(backgroundColor)}>
+// 				<AlertDialogHeader>
+// 					{title && <AlertDialogTitle>{title}</AlertDialogTitle>}
+// 					{description && (
+// 						<AlertDialogDescription>{description}</AlertDialogDescription>
+// 					)}
+// 				</AlertDialogHeader>
+// 				<AlertDialogFooter>
+// 					<AlertDialogCancel>Cancel</AlertDialogCancel>
+// 					<AlertDialogAction>Action</AlertDialogAction>
+// 				</AlertDialogFooter>
+// 			</AlertDialogContent>
+// 		</AlertDialog>
+// 	),
+// };
